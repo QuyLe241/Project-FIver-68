081e9e7a0c826cf2eb4ca33692fba91e9cf5c80f
QUY666666 <lenhatquy2401@gmail.com>
2024-08-14 01:46:53 +0700

    update img n

--- src/page/Register/RegisterPage.jsx
+++ src/page/Register/RegisterPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import FormRegister from "../../components/FormRegister/FormRegister";
+import ImgRegis from "../../Img/RegisterImg1.png";
 
 const RegisterPage = () => {
   return (
     <div className="container">
       <div className="flex">
         <div className="w-1/2">
-          {/* <img src="" alt="img register" /> */}
+          <img src={ImgRegis} alt="img register" />
           {/* <img src="../../../public/RegisterImg.png" alt="" /> */}
-          <img src="./src/Img/RegisterImg1.png" alt="" />
+          {/* <img src="./src/Img/RegisterImg1.png" alt="" /> */}
         </div>
         <div className="w-1/2">
           <FormRegister />
